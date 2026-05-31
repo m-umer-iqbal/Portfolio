ed43cecb7975efdaa6f7181ee87c291103b77c14
Umer Iqbal <official.umer.iqbal@gmail.com>
2026-05-31 17:41:21 +0500

    Change subheading to dynamic

--- app/header.tsx
+++ app/header.tsx
@@ -1,7 +1,31 @@
 'use client'
-import { TextEffect } from '@/components/ui/text-effect'
+
+import { TextEffect } from '@/components/ui/text-effect';
+import { useSearchParams } from "next/navigation";
+
+const roles = {
+  "frontend": "Frontend Web Developer",
+  "backend": "Backend Web Developer",
+  "fullstack": "Full-Stack Web Developer",
+  "mern": "MERN Stack Web Developer",
+  "webd": "Web Developer"
+} as const;
 
 export function Header() {
+  const query = useSearchParams();
+  const role: string = query.get("role") || "web";
+  let value: string = "Web Developer";
+
+  let existedRole: keyof typeof roles;
+
+  for (existedRole in roles) {
+    if (existedRole === role) {
+      value = roles[existedRole]
+      break;
+    }
+  }
+
+
   return (
     <header className="mb-24 flex flex-col items-center text-center bg-white dark:bg-black">
       <div className="flex flex-col items-center gap-2">
@@ -15,15 +39,7 @@ export function Header() {
             per="char"
             className="text-xl text-zinc-600 dark:text-zinc-500 px-2"
             delay={0.5}>
-            Full-Stack Web Developer
-          </TextEffect>
-          <TextEffect
-            as="p"
-            preset="fade"
-            per="char"
-            className="text-xl text-zinc-600 dark:text-zinc-500 italic px-2"
-            delay={1.5}>
-            (MERN & Next.js)
+            {value}
           </TextEffect>
         </div>
       </div>
